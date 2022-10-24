1b1759c495080772e2975cd55e2d694752fd37e1
Alicja Winnicka <alicja.winnicka@q-perior.pl>
2022-10-24 14:52:50 +0200

    Create DataTemplate

--- HarryPotter.MainApplication/HarryPotter.Infrastructure/Model/Character.cs
+++ HarryPotter.MainApplication/HarryPotter.Infrastructure/Model/Character.cs
@@ -8,10 +8,13 @@ namespace HarryPotter.Infrastructure.Model
 {
     public class Character
     {
+        private string? name;
+        private string? gender;
+
         public int Id { get; set; }
-        public string? Name { get; set; }
+        public string? Name { get => name; set => name = value; }
         public string? Species { get; set; }
-        public string? Gender { get; set; }
+        public string? Gender { get => gender; set => gender = value; }
         public string? House { get; set; }
         public string? DateOfBirth { get; set; }
         public int? YearOfBirth { get; set; }
