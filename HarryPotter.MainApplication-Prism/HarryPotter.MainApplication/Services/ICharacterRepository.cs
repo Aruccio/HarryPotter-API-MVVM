2146fa783ed1ae72f123accf97437f4287643f47
SST\awinnicka <alicja.winicka@q-perior.pl>
2022-11-02 09:19:35 +0100

    Duplicate project for prism version

---
+++ HarryPotter.MainApplication-Prism/HarryPotter.MainApplication/Services/ICharacterRepository.cs
@@ -0,0 +1,16 @@
+﻿using HarryPotter.Infrastructure.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarryPotter.MainApplication.Services
+{
+    public interface ICharacterRepository
+    {
+        Task<ICollection<Character>> GetAllCharacters();
+
+    }
+}
---
+++ HarryPotter.MainApplication-Prism/HarryPotter.MainApplication/ViewModel/CharacterListViewModel.cs
@@ -0,0 +1,87 @@
+﻿using HarryPotter.Infrastructure.Model;
+using HarryPotter.MainApplication.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace HarryPotter.MainApplication.ViewModel
+{
+    public class CharacterListViewModel
+    {
+        private ICharacterRepository _repository = new CharacterRepository();
+
+        public CharacterListViewModel()
+        {
+            AddToFavCommand = new RelayCommand(OnAdd, CanAdd);
+            DeleteCommand = new RelayCommand(OnDelete, CanDelete);
+
+        }
+
+        public RelayCommand AddToFavCommand { get; private set; }
+        public RelayCommand DeleteCommand { get; private set; }
+
+        private ObservableCollection<Character> allCharacters = new ObservableCollection<Character>();
+        private Character selectedCharacter;
+        private ObservableCollection<Character> favouriteCharacters = new ObservableCollection<Character>();
+
+        public ObservableCollection<Character> AllCharacters
+        {
+            get { return allCharacters; }
+            set { allCharacters = value; }
+        }
+
+        public ObservableCollection<Character> FavouriteCharacters
+        {
+            get { return favouriteCharacters; }
+            set { favouriteCharacters = value; }
+        }
+
+        public async void LoadAllCharacters()
+        {
+            var chars = await _repository.GetAllCharacters();
+
+            foreach (var character in chars)
+            {
+                allCharacters.Add(character);
+            }
+        }
+
+        public Character SelectedCharacter
+        {
+            get => selectedCharacter;
+            set 
+            { 
+                selectedCharacter = value;
+                DeleteCommand.RaiseCanExecuteChanged();
+                AddToFavCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        private void OnAdd()
+        {
+            FavouriteCharacters.Add(SelectedCharacter);
+        }
+
+        private void OnDelete()
+        {
+            AllCharacters.Remove(SelectedCharacter);
+        }
+
+        private bool CanAdd()
+        {
+            return SelectedCharacter != null;
+        }
+
+        private bool CanDelete()
+        {
+            return SelectedCharacter != null;
+        }
+
+    }
+}
