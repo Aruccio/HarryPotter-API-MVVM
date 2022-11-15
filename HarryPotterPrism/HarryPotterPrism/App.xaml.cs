e3650c79fa4566c39a6cc614be98b39dac28663e
Alicja Winnicka <alicja.winnicka@q-perior.pl>
2022-11-15 16:31:04 +0100

    Run stackpanel custom region

--- HarryPotterPrism/HarryPotterPrism/App.xaml.cs
+++ HarryPotterPrism/HarryPotterPrism/App.xaml.cs
@@ -1,17 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using HarryPotterPrism.Core.Regions;
+using HarryPotterPrism.Views;
+using Prism.Ioc;
+using Prism.Regions;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace HarryPotterPrism
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App
     {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+
+        protected override void ConfigureRegionAdapterMappings(RegionAdapterMappings regionAdapterMappings)
+        {
+            base.ConfigureRegionAdapterMappings(regionAdapterMappings);
+            regionAdapterMappings.RegisterMapping(typeof(StackPanel),
+                Container.Resolve<StackPanelRegionAdapter>());
+        }
     }
 }
